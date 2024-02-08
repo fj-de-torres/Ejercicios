d526c5b1e10a0a22e251251380ffef2080237bc2
Francisco <fj.detorres@gmail.com>
2024-02-08 09:57:23 +0100

    Reframing the problem in a different way

--- session4/par_impar.py
+++ session4/par_impar.py
@@ -3,9 +3,11 @@ from colorama import Fore, Back, Style
 
 odd_num = []
 even_num = []
-def num_request(num):
+""" def num_request(num):
     num = input ("Please, write a number. To finish, press 's' key: ")
     if num % 2 == 0:
         even_num [-1] = num
     else:
         odd_num [-1] = num
+ """
+list = input ("Please, enter a list of numbers separated by comas: ")
