6bb164c072513638cdbe2cb8c2585835f098e00e
Francisco <fj.detorres@gmail.com>
2024-02-08 13:26:19 +0100

    This file added

---
+++ session4/temperatures.py
@@ -0,0 +1,12 @@
+import os
+from colorama import Fore, Back, Style #I'd like to use text colours.
+os.system("cls || clear") #Clear terminal every time I run this program.
+
+"""
+𝗥𝗲𝗴𝗶𝘀𝘁𝗿𝗼 𝗱𝗲 𝗧𝗲𝗺𝗽𝗲𝗿𝗮𝘁𝘂𝗿𝗮𝘀
+Imagina que estás desarrollando un programa para una estación
+meteorológica. Este programa necesita registrar las temperaturas máxima y
+mínima de cada día de la semana. Al final de la semana, el programa debe
+ser capaz de mostrar la temperatura máxima y mínima registrada durante
+la semana, además de calcular la temperatura promedio.
+"""
