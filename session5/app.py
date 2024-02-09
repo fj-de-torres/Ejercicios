4cc976635f5ef860a09a46f2bb9f46060ec536b0
Francisco <fj.detorres@gmail.com>
2024-02-09 13:11:03 +0100

    Teacher's solution. Mine deleted

--- session5/app.py
+++ session5/app.py
@@ -1,6 +1,6 @@
 import os
 os.system ("cls || clear")
-#from colorama import 
+from colorama import Fore, Back, Style
 #Tratamiento de clientes:
 #DNI, Nombre, Apellido, CP.
 
@@ -35,12 +35,18 @@ def eliminar_cliente(clave: str, diccio: dict):
     if clave in diccio:
         diccio.pop(clave)
     return diccio
-def buscar_cliente():
-    pass
-def mostrar_cliente(d_clientes:dict):
-    if d_clientes is not None:
-        for dni, _ in d_clientes.items():
-            print(dni)
+
+def buscar_cliente(dni_cliente:str, d_clientes:dict) -> dict:
+    if dni_cliente not in d_clientes: return #Esto devuelve un None si el DNI no existe. Salgo de la función
+    return d_clientes[d_clientes]
+
+def buscar_cliente(dni_cliente: str, d_clientes: dict) -> dict:
+    return d_clientes.get(dni_cliente)
+"""     
+    if dni_cliente not in d_clientes: return #None
+    return d_clientes[dni_cliente]
+ """
+    
           # print (_) #dará el resto; o sea, el diccioario de cada cliente
 # Aclaración
 # dni, *_ = [(1,2,3,4),(8,9,7,6), (1,2,3,4,5)]
@@ -74,5 +80,14 @@ if __name__ == "__main__":
     
     # print(f"Clientes al inicio: {clientes}")
     # print(f"Clientes eliminado 222H: {clientes}")
-    clave = '222H'
-    clientes= eliminar_cliente(clave, clientes)
+    clave = '333H'
+    #clientes= eliminar_cliente(clave, clientes)
+
+    #Preguntar al usuario el dni y buscar dicho cliente
+    cliente = buscar_cliente(input('DNI a buscar:'),clientes)
+    if cliente:
+        print(list(cliente.items())[1:])
+        print(dict(list(cliente.items())[1:])) #Aquí vuelve a ser diccionario.
+
+    else:
+        print("No existe")
