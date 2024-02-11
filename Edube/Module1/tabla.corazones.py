3ab6ecb79b417a2fab8a94e9ec316d4b9f50e2e0
Francisco <fj.detorres@gmail.com>
2024-02-11 18:13:38 +0100

    New folder and exercise added. Pdf removed

---
+++ Edube/Module1/tabla.corazones.py
@@ -0,0 +1,20 @@
+for i in range(4):
+    for i in range(3):
+        for i in range(8):
+            print("💝",end="")
+    print("💝")
+    for i in range(3):
+        for i in range(3):
+            for i in range(2):
+                print("💖",end="")
+                for i in range(3):
+                    print(" ",sep=" ",end=" ")
+        print("💖")
+    #for i in range(3):
+    #    for i in range(10):
+    #        print("💝",end="")
+    #print()
+for i in range(3):
+        for i in range(8):
+            print("💝",end="")
+print("💖")
