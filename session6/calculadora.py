65026ddd6541752945baf80a3e86d94a3915d3c0
Francisco <fj.detorres@gmail.com>
2024-02-12 12:01:11 +0100

    Adding more info

--- session6/calculadora.py
+++ session6/calculadora.py
@@ -3,3 +3,10 @@ def sumar(x: int, y: int) -> int:
 
 def restar (x:int, y:int) -> int:
     return x - y
+
+def sumar_v2(*numeros) -> int: # Poniendo ese * delante de numeros, se trata como tupla. Permite un nº indeterminado de parámetros de entrada.
+#En otros lenguajes, esto es a lo que se le llama un *VarArgs*
+    return sum(numeros) #sum es una función de Python que es un sumatorio
+
+def sumar_v3(*numeros, otro_param:str='-') -> int: #varargs
+    return sum(numeros)
