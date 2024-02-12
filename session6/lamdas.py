92f6b0c89d541c956971224be24fa9c6a5bbc86c
Francisco <fj.detorres@gmail.com>
2024-02-12 12:36:05 +0100

    More optiones to lambda filter added

--- session6/lamdas.py
+++ session6/lamdas.py
@@ -12,7 +12,7 @@ r1 = sumar_lambda(10,3)
 r2 = sumar(10,3)
 
 print(r1 == r2)
-sumar_lamda2 = lamda x, y : calcular_algo_complejo() # De esta manera, podría definir funciones lamda de más de una línea de código, pouesto que en su única línea de código estoy invocando una función que sí que tiene más de una línea.
+sumar_lamda2 = lambda x, y : calcular_algo_complejo() # De esta manera, podría definir funciones lamda de más de una línea de código, pouesto que en su única línea de código estoy invocando una función que sí que tiene más de una línea.
 
 ###### Otro ejemplo:
 
@@ -22,9 +22,29 @@ def calcular(x:int, y: int, d:dict):
     res = proceso1(10,1)
 
 #### Funcionalidades lambda:
+    
+#Defino esta función para "descomplicar" las condiciones que impondré al *filter lambda*
+def mayor_siete(alumno:tuple):
+    nota = alumno[1]
+    return nota > 7.0
 ##### map -> transformación: un arreglo a la función alumnos con nota x * .5 (arreglo un resultado)
 ##### filtrado -> filtrado: alumnos que han obtenido una nota > 7. Me devuelve una colección que cumple las condiciones del filtrado.
 
-alumnos = [('maite,6.0'), ('alicia', 8.0), ('luis', 7.5), ('juan',7.0)]
+alumnos = [('maite',6.0), ('alicia', 8.0), ('luis', 7.5), ('juan',7.0)]
 
-alumnos_mas_siete = filter()
+alumnos_mas_siete = list(filter(lambda alumno: alumno[-1] > 7.0, alumnos)) #Quiero la segunda posición, luego, lo mismo me vale [1] que [-1]
+#Es decir, estoy aplicando una condicíon en forma de lambda. Es lo que antes se haría con un bucle for y un if.
+#Tenemos que convertir a list porque el resultado del filter es un elemento *filter* que no nos sirve. Necesitamos una lista que sea el resultado de filtrar en otra *lista*
+
+print(type(alumnos_mas_siete))
+print(alumnos_mas_siete)
+
+#La condición del filtro la puedo complicar:
+alumnos_mas_siete = list(filter(lambda alumno: alumno[1] > 7.0 and alumno[1] < 8.0 , alumnos))
+print(alumnos_mas_siete)
+
+#Descomplico lo anterior si he definido una función a parte (mayor_siete) que será parte de la condición del filtro de la función *lambda*:
+
+alumnos_mas_siete = list(filter(lambda alumno: mayor_siete(alumno) , alumnos))
+print(type(alumnos_mas_siete))
+print(alumnos_mas_siete)
