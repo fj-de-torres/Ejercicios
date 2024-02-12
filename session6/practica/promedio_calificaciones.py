0fb87617140e8d18a72d596ba9674689093fe1db
Francisco <fj.detorres@gmail.com>
2024-02-12 20:07:07 +0100

    I've just writen the statements of the problem to be solved

---
+++ session6/practica/promedio_calificaciones.py
@@ -0,0 +1,25 @@
+"""
+# Promedio de Calificaciones por Asignatura
+### Se desea escribir un programa que calcule el promedio de calificaciones de los estudiantes en diferentes asignaturas. Cada asignatura y las calificaciones de los estudiantes para esa asignatura serán ingresadas por el usuario. El programa debe ser capaz de mostrar el promedio de calificaciones para cada asignatura.
+
+## Objetivos:
+1. Permitir al usuario ingresar múltiples asignaturas y las calificaciones correspondientes de los estudiantes.
+2. Calcular el promedio de calificaciones para cada asignatura.
+3. Mostrar el promedio de calificaciones por asignatura.
+"""
+import os
+from colorama import Fore, Back, Style
+
+os.system("cls || clear")
+
+### 1. Opción 1. Pido nombre de cada estudiante y, a continuación, una asignatura y una nota
+### 2. Opción 2. La lista de estudiantes es conocida y sólo se necesitan las asignaturas y sus notas. 
+
+### 3. Opción 3. Me centro en obtener conjuntos (diccionarios) de asignatura - notas.
+        #1. Pido nombre de asignatura
+        #2. Pido lista de notas para esa asignatura.
+        #3.
+### 4. Calcular el promedio de las calificaciones (¿average()?) para cada lista de asignaturas
+### 5. Mostrar el promedio el promedio obtenido (print con formato de average)
+
+### 3. Opción 3. Me centro en obtener conjuntos (diccionarios) de asignatura - notas.
