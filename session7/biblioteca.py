b0d16a74d8caf3b607c0f738bf5a56cbff6c6cd3
Francisco <fj.detorres@gmail.com>
2024-02-13 12:26:44 +0100

    Cursive for the md format added

--- session7/biblioteca.py
+++ session7/biblioteca.py
@@ -1,5 +1,5 @@
 """
-Enunciado: Biblioteca
+*Enunciado: Biblioteca
 Imagina que tienes un diccionario que almacena información sobre libros en una biblioteca. Cada clave del diccionario es el título del libro (una cadena de texto), y el valor es una tupla que contiene el nombre del autor y el año de publicación.
 Tu tarea es escribir una función que reciba este diccionario y un año de publicación. La función debe retornar un nuevo diccionario con los libros  publicados después del año proporcionado. Para filtrar los libros, debes usar una función lambda dentro de tu solución.
 Ayuda:
@@ -8,7 +8,7 @@ Ayuda:
 Puedes acceder a sus elementos por índice, por ejemplo, tupla[0] para el nombre del autor y tupla[1] para el año de publicación.
 3. Funciones lambda: Una función lambda en Python es una pequeña función anónima. Puedes usarla para crear funciones rápidas que no necesiten ser definidas con un nombre. Por ejemplo, lambda x: x * 2 es una función que duplica su entrada.
 4. Filtrado con funciones lambda: Puedes combinar funciones lambda con la función filter() para filtrar elementos de una colección. Sin embargo, en este caso, como trabajamos con diccionarios, podrías usar una comprensión de diccionario con una condición que utilice la función lambda.
-5. Comprensión de diccionarios: La comprensión de diccionarios te permite crear un nuevo diccionario a partir de iterar sobre un diccionario existente. La sintaxis general es {clave: valor for (clave, valor) en diccionario.items() if condición}.
+5. Comprensión de diccionarios: La comprensión de diccionarios te permite crear un nuevo diccionario a partir de iterar sobre un diccionario existente. La sintaxis general es {clave: valor for (clave, valor) en diccionario.items() if condición}.*
 """
 
 libros = {
