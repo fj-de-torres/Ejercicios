a3dcce2d2eb5041cd32336da49b07526504049d1
Francisco <fj.detorres@gmail.com>
2024-02-13 10:07:16 +0100

    functions defined

--- session6/practica/calculos.py
+++ session6/practica/calculos.py
@@ -1,4 +1,9 @@
-def calcular_promedio(todo):
-    for t in todo:
-        
-def calcular_promedio_por_asignatura()
+def calcular_promedio_asignatura(asignaturas:dict) -> list:
+    lista_promedios = list() #Quiero crear una lista de tuplas como resultado
+    for asignatura, calificaciones in asignaturas.items():
+        promedio_asignatura = calcular_promedio_por_asignatura(calificaciones)
+        lista_promedios.append(asignatura,promedio_asignatura)
+    return lista_promedios
+
+def calcular_promedio(lista:list) -> float: #No especifico en el nombre que sea lista de asignaturas porque me puede venir bien esta función para el promedio de cualquier otra cosa.
+    return sum(lista) / len(lista)
