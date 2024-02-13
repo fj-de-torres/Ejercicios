6949eb7495eded8f2bb3329a85c590c6ab8d3484
Francisco <fj.detorres@gmail.com>
2024-02-13 11:43:01 +0100

    combining map form lambda in conprehension lists

--- session7/app.py
+++ session7/app.py
@@ -21,3 +21,17 @@ numeros_20_actualizado_v2 = [elevar_cuadrado(numero) for numero in numeros_20]
 print(numeros_20_actualizado_v2)
 numeros_20_actualizado_v3 = [(lambda n : n ** 2) (numero) for numero in numeros_20] #No es lo aconsejable, aunque técnicamente viable.
 print(numeros_20_actualizado_v3)
+
+#### Procesar, de numeros_20, sólo los impares:
+es_impar = lambda n: n%2 != 0
+numeros_20_impares  = [numero -1 for numero in numeros_20 if numero % 2 != 0] #=> numero -1 es simplemente un cálculo que he querido hacer, no tiene nada que ver con el cálculo sobre los impares, que es otra cosa que se pretende aquí.
+numeros_20_impares_v2 = [numero -1 for numero in numeros_20 if es_impar(numero)]
+
+print(numeros_20_impares)
+print(numeros_20_impares_v2)
+
+numeros_20_cerocinco = list(map(lambda n : n + 0.5 , numeros_20))
+print(numeros_20_cerocinco)
+#print(numeros_20_impares)
+
+print([numero for numero in map(lambda n : n + 0.5 , numeros_20)])
