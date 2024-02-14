6d30b66af491cfa8c9366c46ae5ed6e707400e55
Francisco <fj.detorres@gmail.com>
2024-02-14 12:15:53 +0100

    Class exercise and comments added

--- session8/app.py
+++ session8/app.py
@@ -1,12 +1,20 @@
 import os
 from ficheros import leer_data
-os.system("cls || clear")
-def mostrar_alumnos(l_alumnos: list):
+
+def mostrar_alumnos(l_alumnos: str) -> list:
+    lista_alumnos = list()
     for alumno in l_alumnos:
-        print(alumno)
+        #print(alumno)
+        lista_alumnos.append(tuple(alumno.split()))
+    return lista_alumnos
+    # Tratar de hacerlo con un comprehension list
+
 
 if __name__ == "__main__":
+    os.system("cls || clear")
     nombre_alumnos = leer_data('alumnos.txt')
-    
     mostrar_alumnos(nombre_alumnos)
-    
+
+##### Crear una lista de tuplas, cada una formadas por el nombre y el apellido:
+
+    print(mostrar_alumnos(nombre_alumnos))
--- session8/ficheros.py
+++ session8/ficheros.py
@@ -1,9 +1,20 @@
-# Ficheros
+# Fichero
 import os
 #Where the Hell am I?
 ruta_directorio = os.path.dirname(__file__)
 #More info: https://docs.python.org/3/library/pathlib.html
 ## Elemento que nos permite persistencia.
+
+def sanitizar_cadena(cadena:str) -> str: #Devolvemos la cadena limpia
+    return cadena.strip() #De momento quito todo a izda y derecha (rstrip() si sólo quiero a la derecha)
+
+def sanitizar_data (data: list) -> list:
+    data_limpia = list()
+    for cadena in data:
+        data_limpia.append(sanitizar_cadena(cadena))
+    else: #Este 'else' sólo se ejecuta si el for se ha ejecutado completamente (si ha conseguido ejecutar el recorrido completo del FOR). Con que se ejecute un break dentro del for, el 'else' ya no se ejecutaría. Me estoy asegurando de que el for se esté ejecutando correctamente.
+        return data_limpia
+
 
 def escribir_data(nombre_fichero: str, *mensajes):
     with open(f'{ruta_directorio}/data/{nombre_fichero}', 'w') as f:
@@ -16,7 +27,7 @@ def leer_data(nombre_fichero: str) -> str:
     data = None
     try:
         with open(f"{ruta_directorio}/data/{nombre_fichero}", 'r') as f:
-            data = f.readlines() #Y esto devolverá una lísta de lo leído en cada línea
+            data = sanitizar_data(f.readlines()) #Y esto devolverá una lísta de lo leído en cada línea
 
     except FileNotFoundError as fnfex:
         print(fnfex)
