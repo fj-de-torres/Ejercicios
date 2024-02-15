8e25074b7ef4196b6082d28b698cfaf8d7f09afd
Francisco <fj.detorres@gmail.com>
2024-02-15 14:15:06 +0100

    clientes updated. api_rest.py created

---
+++ session9/api_rest.py
@@ -0,0 +1,10 @@
+# Accessing APIs:
+import requests
+#If requests is not already installed, we install it as: ***pip install requests***
+#Python default repositories:
+#   1. pypi.org
+#We can install as a module as well with:
+
+""" 
+***python -m pip install requests***
+"""
--- session9/clientes.py
+++ session9/clientes.py
@@ -2,6 +2,7 @@ import os
 header  = None
 
 def cargar_clientes() -> list:
+    global header
     path_data = f"{os.path.dirname(__file__)}/data/clientes.csv"
     with open('./data/clientes.csv','r') as f:
         #El propio ***f*** con el que abro el archivo también es un iterable
@@ -13,15 +14,27 @@ def cargar_clientes() -> list:
         lineas = f.readlines()
         header = lineas[0] #Variable local
         return [linea.strip().split(',') for linea in lineas[1:]]
-
+""" 
 def guardar_clientes(l_clientes):
-    path_data = f"{os.path.dirname(__file__)}/data/clientes_out.csv"
+    path_data = f"{os.path.dirname(__file__)}/data/clientes.csv"
     with open(path_data, 'w') as f:
         clientes = list()
         for cliente in l_clientes:
             clientes.append(",".join(clientes))
-        #else: #Este else ocurre si acabamos el for de forma completa
-        f.writelines(clientes)
+        else: #Este else ocurre si acabamos el for de forma completa
+            clientes.insert(0,header)
+            f.writelines(clientes)
+ """
+
+def guardar_clientes(l_clientes):
+    path_data = f"{os.path.dirname(__file__)}/data/clientes.csv"
+    with open(path_data, 'w') as f:
+        clientes = list()
+        for cliente in l_clientes:
+            clientes.append(f'{",".join(cliente)}\n')
+        else:
+            clientes.insert(0, header)
+            f.writelines(clientes)
 
 def main():
     clientes = cargar_clientes()
