8fa5813619d5886feec50fcabf966424167ca34e
Francisco <fj.detorres@gmail.com>
2024-02-15 10:33:48 +0100

    created structure for the practice made by the teacher

--- session9/practica_gestion_tareas/app.py
+++ session9/practica_gestion_tareas/app.py
@@ -1,5 +1,8 @@
+from gestion_ficheros.ficheros import leer_data, escribir_data
 def main():
-    pass
+    #Ingesta de datos
+    tareas = leer_data('tareas.txt')
+    print(tareas)
 
 if __name__== "__main__":
     main() #Llama al programa principal en vez de andar soltando código aquí
