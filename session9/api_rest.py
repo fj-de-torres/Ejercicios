8fea7dacd5a03b04ddd3af51a40a3d5169a0929c
Francisco <fj.detorres@gmail.com>
2024-02-16 09:23:08 +0100

    --

--- session9/api_rest.py
+++ session9/api_rest.py
@@ -29,15 +29,19 @@ print(response.text)
 
 ### Bajar los usuarios y guardarlos en un fichero en formato csv (id, username, email, website):
 
+"""This is going to be the header of the file."""
 head = ['id' , 'username' , 'email' , 'website']
+
+"""This is going to be the default file if no one is specified by the user:"""
 default_file = "./data/user.csv" #Set as default path/file.csv
+
 """To let the user choose a file where to store the read data:"""
 def fetch_url_from_user():
       file_to_open = input(Style.BRIGHT + "Please enter the url to fetch users from: " + Style.RESET_ALL)
       return file_to_open if file_to_open != ("" or None) else default_file
 
-csv_file = default_file
 """The option I choose at tme moment just for testing, is cvs_file as the default_file. Otherwise, cvs_file = fetch_url_from_user()"""
+csv_file = default_file
 #cvs_file = fetch_url_from_user()
 def get_users (url:str):
     """
@@ -52,10 +56,16 @@ def get_users (url:str):
     return [data,multi_user]
     # print(data)
     # print(len(data))
+def user_to_str(user:list) -> str:
+    if user [1] == False:
+        user_str = user[0][0] + "," + user[0][1] + "," + user[0][4] + "," + user[0][5] + "," + user[0][6] + "," + user[0][7]
+    else:
+        x = url[-1]
+        user_str = user[x][0][0] + "," + user[x][0][1] + "," + user[x][0][4] + "," + user[x][0][5] + "," + user[x][0][6] + "," + user[x][0][7]
 
 def create_csv_file(file_to_open:str):
     with open (file_to_open,'w') as file_csv:
-                file_csv.write(f"{head}\n")
+        file_csv.write(f"{head}\n")
     #file_to_open = input("Enter name for csv file with relative path: ")
                 
 def append_data(json_data:str):
@@ -63,11 +73,15 @@ def append_data(json_data:str):
     try: #if you don't mind
         file_to_append =open (csv_file,'r')
         if first_line == file_to_append.readlines()[0]:
-            pass
+            line_str = user_to_str(json_data)
+        else:
+            line_str = head + "\n" + user_to_str(json_data)
     except FileNotFoundError: #you'd better exist or you'll be replaced!
             create_csv_file() 
 
-print(get_users('https://jsonplaceholder.typicode.com/users/1'))
+
+json_users = get_users('https://jsonplaceholder.typicode.com/users/1')
 create_csv_file('data/user.csv')
+append_data_(json_users)
 
 
