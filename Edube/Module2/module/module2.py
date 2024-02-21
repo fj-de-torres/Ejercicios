a627feb1353de09e96d60da43ca31d636ea3f001
Francisco <fj.detorres@gmail.com>
2024-02-21 22:10:49 +0100

    Example files added

--- Edube/Module2/module/module2.py
+++ Edube/Module2/module/module2.py
@@ -1,13 +1,14 @@
 from sys import path
-
-path.append('..\\packages\\extrapack.zip')
-
+import os
+os.system("cls || clear")
+#os.system("export $PYTHONPATH = ~/Documents/Learning/Python/Ejercicios/Edube/Module2/")
+path.append("./extrapack.zip")
 import extra.good.best.sigma as sig
 import extra.good.alpha as alp
-from extra.iota import funI
-from extra.good.beta import funB
+from extra.iota import FunI as funI
+from extra.good.beta import FunB as funB
 
-print(sig.funS())
-print(alp.funA())
+print(sig.FunS())
+print(alp.FunA())
 print(funI())
 print(funB())
