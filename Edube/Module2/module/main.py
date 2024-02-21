809603ac32d58f9b451b4b005d7a0b297315eb4f
Francisco <fj.detorres@gmail.com>
2024-02-21 20:09:19 +0100

    updated

--- Edube/Module2/module/main.py
+++ Edube/Module2/module/main.py
@@ -1,1 +1,15 @@
-import module
+import os, sys
+
+"""
+os.system("cls || clear")
+print("Todo correcto")
+print(__name__) """
+#print(module.counter)
+from module import suml, prodl
+
+zeroes = [0 for i in range(5)]
+ones = [1 for i in range(5)]
+print(suml(zeroes))
+print(prodl(ones))
+for p in sys.path:
+    print(p)
