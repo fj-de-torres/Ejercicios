c3dda95fefd07b72eb7c648921045bffca2ee8b8
Francisco <fj.detorres@gmail.com>
2024-02-23 21:36:03 +0100

    seven_segments changed to personalized chars and keyboard_interruptus.py added

---
+++ Edube/Module2/keyboard_interruptus.py
@@ -0,0 +1,18 @@
+﻿from time import sleep
+def division():
+    try:
+        x = int(input("Ingresa un numero: "))
+        y = 1 / x
+        print(y)
+    except ZeroDivisionError:
+        print("No puedes dividir entre cero, lo siento.")
+    except ValueError:
+        print("Debes ingresar un valor entero.")
+    except KeyboardInterrupt:
+        print("¡Vamos, no me jodas!")
+        sleep(1)
+        division()
+    except:
+        print("Oh cielos, algo salió mal...")
+division()
+print("FIN.")
--- Edube/Module2/seven_segment.py
+++ Edube/Module2/seven_segment.py
@@ -1,19 +1,24 @@
 from os import system
 system("cls || clear")
-leds = {
-    1 : ['  #','  #','  #','  #','  #'],
-2 : ['###','  #','###','#  ','###'],
-3 : ['###','  #','###','  #','###'],
-4 : ['# #','# #','###','  #','  #'],
-5 : ['###','#  ','###','  #','###'],
-6 : ['###','#  ','###','# #','###'],
-7 : ['###','  #','  #','  #','  #'],
-8 : ['###','# #','###','# #','###'],
-9 : ['###','# #','###','  #','###'],
-0 : ['###','# #','# #','# #','###']
-}
 
-def led_number(n:str):
+
+def led_number(n:str,char="#"):
+    full = '■'*3
+    c_s_c = char+" " + char
+    s_s_c = " "*2 + char
+    c_s_s = char + " " * 2
+    leds = {
+    1 : [s_s_c for i in range(5)],
+2 : [full,s_s_c,full,c_s_s,full],
+3 : [full,s_s_c,full,s_s_c,full],
+4 : [c_s_c,c_s_c,full,s_s_c,s_s_c],
+5 : [full,c_s_s,full,s_s_c,full],
+6 : [full,c_s_s,full,c_s_c,full],
+7 : [full,s_s_c,s_s_c,s_s_c,s_s_c],
+8 : [full,c_s_c,full,c_s_c,full],
+9 : [full,c_s_c,full,s_s_c,full],
+0 : [full,c_s_c,c_s_c,c_s_c,full]
+}
     n = str(n)
     # for digito in n:
     #     if digito.isdigit():
@@ -22,10 +27,9 @@ def led_number(n:str):
     
     for i in range(5):
         for numero in n:
-            print(leds[int(numero)][i],end=" ")
+            print(leds[int(numero)][i],end="  ")
         print()
     print()
-
-led_number(123)
+led_number(123,'❚')
 #print(leds['1'])
-led_number(9081726354)
+led_number(9081726354,'❚')
