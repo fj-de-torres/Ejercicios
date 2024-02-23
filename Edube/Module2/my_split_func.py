4c6404ad2a289d42fed840d856c0e5f3860fcf9d
Francisco <fj.detorres@gmail.com>
2024-02-23 12:46:05 +0100

    Now empty strings or made by spaces give me [] instead of ['']. As the exercise statement requires

--- Edube/Module2/my_split_func.py
+++ Edube/Module2/my_split_func.py
@@ -7,15 +7,23 @@ def my_split(input_string: str,custom_char=" ")->list:
         char_1 = chr(i)
         if not char_1.isalnum() or char_1 == "·":
             input_string = input_string.strip(char_1)
-    for char_2 in input_string:
-        if char_2 != custom_char:
-            tmp_chain += char_2
-        elif char_2 == custom_char:
-            def_list.append(tmp_chain)
-            tmp_chain =""
-    def_list.append(tmp_chain)
+    if len(input_string) == 0:
+        def_list = []
+    else:
+        for char_2 in input_string:
+            if char_2 != custom_char:
+                tmp_chain += char_2
+            elif char_2 == custom_char:
+                def_list.append(tmp_chain)
+                tmp_chain =""
+        def_list.append(tmp_chain)
     
     return def_list
-testing_string = input("Gimme a string! ")
-testing_separator=input("Spliting/separator character (space by default)? ")
-print(my_split(testing_string,testing_separator))
+# testing_string = input("Gimme a string! ")
+# testing_separator=input("Spliting/separator character (space by default)? ")
+# print(my_split(testing_string,testing_separator))
+print(my_split("Ser o no ser, esa es la pregunta"))
+print(my_split("Ser o no ser,esa es la pregunta"))
+print(my_split("   "))
+print(my_split(" abc "))
+print(my_split(""))
