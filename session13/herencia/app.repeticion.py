5868cb9132429aef7de0edfd0cc43fded218490a
Francisco <fj.detorres@gmail.com>
2024-02-28 22:37:27 +0100

    add_columna added

--- session13/herencia/app.repeticion.py
+++ session13/herencia/app.repeticion.py
@@ -1,4 +1,5 @@
 from os import system
+from funfont import *
 system("cls || clear")
 
 class A:
@@ -67,3 +68,6 @@ linea(times=100)
 print(Fore.LIGHTGREEN_EX + "Ｏｒ．．． ｗｉｔｈ Ｐｒｅｔｔｙｔａｂｌｅ：" + Fore.WHITE)
 print(results2)
 linea(times=10)
+system("clear")
+
+#print(columna)
