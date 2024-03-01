61ce7c68dd940bfb5642cb9a2606453e130e40c4
Francisco <fj.detorres@gmail.com>
2024-03-01 10:04:23 +0100

    imports added

--- session15/app.py
+++ session15/app.py
@@ -1,2 +1,6 @@
-from pathlib import Path
+from sys import path
+path.append('/home/francisco/Documents/Learning/PUE/Python/Ejercicios/')
+from funfont import *
+from colorama import Fore,Back, Style
+from os import system
 
