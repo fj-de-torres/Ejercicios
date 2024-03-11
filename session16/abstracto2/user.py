700af92e462045ffc1852e177fd9023b7df2398a
Francisco <fj.detorres@gmail.com>
2024-03-11 14:02:12 +0100

    updated

--- session16/abstracto2/user.py
+++ session16/abstracto2/user.py
@@ -1,1 +1,17 @@
 #Una interfaz es una clase abstracta en el que todos los métodos son abstractos. Existe en otros lenguajes.
+class User(Saveable):
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+    
+    def login(self):
+        return 'Logged in!'
+    
+    def __repr__(self):
+        return f'<User {self.username}>'
+
+    def to_dict(self):
+        return {
+            'username': self.username,
+            'password': self.password
+        }
